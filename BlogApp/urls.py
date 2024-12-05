b205037ba56ba72bd4853d61f63e4e2271caaa59
Ranshari Rodrigo <ransharirodrigo@gmail.com>
2024-12-05 23:27:06 +0530

    Registration page design

--- BlogApp/urls.py
+++ BlogApp/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from users.views import register
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("",register,name="register"),
 ]
